4974f154162e9b49b22b8c2295d43d4b734abd14
yoonamessi123 <1994238842@qq.com>
2023-05-31 13:27:57 +0800

    update service.go

--- service/service.go
+++ service/service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"go/ast"
+	"log"
 	"reflect"
 	"sync/atomic"
 )
@@ -15,4 +17,45 @@ type methodType struct {
 
 func (mt *methodType) NumCalls() uint64 {
 	return atomic.LoadUint64(&mt.numCalls)
+}
+func (mt *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+	if mt.ArgType.Kind() == reflect.Ptr {
+		argv = reflect.New(mt.ArgType.Elem())
+	} else {
+		argv = reflect.New(mt.ArgType).Elem()
+	}
+	return argv
+}
+func (mt *methodType) newReply() reflect.Value {
+	replyv := reflect.New(mt.ReplyType.Elem())
+	switch mt.ReplyType.Kind() {
+	case reflect.Map:
+		replyv.Elem().Set(reflect.MakeMap(mt.ReplyType.Elem()))
+	case reflect.Slice:
+		replyv.Elem().Set(reflect.MakeSlice(mt.ReplyType.Elem(), 0, 0))
+	}
+	return replyv
+}
+
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+func NewService(rcvr interface{}) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc server: %s is not a valid service name", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+func (s *service) registerMethods() {
+	
 }
